fef319f1a82d2899c9fe5dd45e004370d0576812
gonzalesraphael <raphael__001@hotmail.com>
2025-11-26 15:25:36 -0300

    Corrigir referência do mongo no arquivo profile

--- api/profile/[email].cjs
+++ api/profile/[email].cjs
@@ -1,4 +1,4 @@
-const { connectToDatabase } = require("./mongo");
+const { connectToDatabase } = require("../mongo");
 
 module.exports = async (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
